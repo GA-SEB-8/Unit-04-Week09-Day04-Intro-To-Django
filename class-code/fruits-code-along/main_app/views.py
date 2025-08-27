74f9a70e6853a4f2023d1a521e349b9fcb312436
omarakamal <omaraakamal@gmail.com>
2025-08-27 11:23:10 +0300

    update

--- class-code/fruits-code-along/main_app/views.py
+++ class-code/fruits-code-along/main_app/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def welcome(request):
     
     return render(request,'welcome.html')
+
+# 1. create the view function
+# 2. add the view function to your urls.py
+# 3. create the html file
